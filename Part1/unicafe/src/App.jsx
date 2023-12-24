cb0c8c8ff4827bf77e9679284a8728e69dc001e5
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-12-24 02:06:11 -0500

    Excersize 1.6

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => (
+  
+  <div>
+    <Heading text='statistics' />
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </div>
+)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+const Heading = ({text}) => <h1>{text}</h1>
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+  
+
+
   return (
     <div>
-      code here
+        <Heading text='give feeedback' />
+        <Button handleClick={handleGoodClick} text='good' />
+        <Button handleClick={handleNeutralClick} text='neutral' />
+        <Button handleClick={handleBadClick} text='bad' />
+        <Statistics good={good} neutral={neutral} bad={bad}  />
     </div>
   )
 }
