35df3f96ef432ce1ae1430357b2d4dfee295e832
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-12-25 16:07:51 -0500

    Thephonebook  2.12

--- Part2/thephonebook/src/App.jsx
+++ Part2/thephonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import Personform from './components/Personform'
+import personService from './services/persons'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    personService
+  .getAll()
+  .then(initialPersons => {
+    setPersons(initialPersons)
+  })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -27,7 +31,11 @@ const App = () => {
         number: newNumber,
         id: persons.length + 1 
       }
-      setPersons(persons.concat(personObject))
+      personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+      })
    }    
 
     setNewName('')
