d256d4c60305c9c7d88f4d9962cd9c194cdd9083
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-01-09 11:46:17 -0500

    Exercises 6.15

--- Part6/redux-notes/src/main.jsx
+++ Part6/redux-notes/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
-
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+
 
 const store = configureStore({
   reducer: {
@@ -13,8 +13,8 @@ const store = configureStore({
     filter: filterReducer
   }
 })
-console.log(store.getState())
 
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
