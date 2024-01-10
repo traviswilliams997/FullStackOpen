a1349d52a377201888e4932e0a48cf7ae1cda3d0
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-01-09 23:58:38 -0500

    Exercise 6.24

--- Part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ Part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,9 @@
 import {  useMutation, useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../requests'
+import { useNotificationDispatch } from '../NotificationContext'
 
 const AnecdoteForm = () => {
+  const notificationDispatch = useNotificationDispatch()
   const queryClient = useQueryClient()
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote, 
@@ -10,11 +12,28 @@ const AnecdoteForm = () => {
     },
   })
 
+  const handleErrorNotification= (message) => {
+
+    notificationDispatch({
+      type: 'SET',
+      payload: `${message}`
+    })
+    setTimeout(() => {
+      notificationDispatch({
+        type: 'CLEAR',
+      })
+    }, 5000)
+  }
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
+
+    if( newAnecdoteMutation.failureCount > 0 || content.length < 5) {
+      handleErrorNotification('too short anecdote, must have length 5 or more') 
+   }
+    
 }
 
   return (
